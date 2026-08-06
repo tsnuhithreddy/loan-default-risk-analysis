c7fb313c829c8884ecba8fa50f11fd79a42cb23e
Snuhith Reddy <snuhith123@gmail.com>
2026-08-06 19:19:45 +0530

    Add SQL database setup script

---
+++ sql/setup.sql
@@ -0,0 +1,45 @@
+-- Loan Default Risk Analysis
+-- Phase 1: Database Setup
+
+-- IF NOT EXISTS prevents errors if script is run multiple times
+CREATE DATABASE IF NOT EXISTS fintrust_lending;
+USE fintrust_lending;
+
+-- Create main table
+
+CREATE TABLE IF NOT EXISTS credit_risk (
+    person_age INT,
+    person_income INT,
+    person_home_ownership VARCHAR(20),
+    person_emp_length FLOAT,
+    loan_intent VARCHAR(30),
+    loan_grade VARCHAR(5),
+    loan_amnt INT,
+    loan_int_rate FLOAT,
+    loan_status INT,
+    loan_percent_income FLOAT,
+    cb_person_default_on_file VARCHAR(5),
+    cb_person_cred_hist_length INT,
+    is_unemployed INT
+);
+
+-- Verify table
+
+SHOW TABLES;
+DESCRIBE credit_risk;
+
+-- Data imported using MySQL Workbench
+-- File: credit_risk_cleaned.csv
+-- Records imported: 32,416
+
+-- Verify import
+
+SELECT COUNT(*) AS total_rows
+FROM credit_risk;
+
+-- Preview data
+
+SELECT *
+FROM credit_risk
+LIMIT 10;
+
